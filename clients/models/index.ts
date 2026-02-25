eb70e4f3054efdeaf476b0b7f5238ad82c5ee4f0
tsilavina andriamiharison <136437326+Tsilavina007@users.noreply.github.com>
2026-02-25 07:21:52 +0100

    feat: implement project ressource

--- clients/models/index.ts
+++ clients/models/index.ts
@@ -2,6 +2,8 @@
 /* eslint-disable */
 export * from "./CreationGoal";
 export * from "./CreationLabel";
+export * from "./CreationProject";
+export * from "./CreationProjectTransaction";
 export * from "./CreationTransaction";
 export * from "./CreationWallet";
 export * from "./Credentials";
@@ -11,6 +13,9 @@ export * from "./GetAllWallets200Response";
 export * from "./Goal";
 export * from "./Label";
 export * from "./PaginationResult";
+export * from "./Project";
+export * from "./ProjectStatistics";
+export * from "./ProjectTransaction";
 export * from "./SignInResult";
 export * from "./SignUpResult";
 export * from "./Transaction";
